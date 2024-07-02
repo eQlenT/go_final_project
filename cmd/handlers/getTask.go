c96c84708efabf8905595d8c9bcbd30f04e7a450
ibratash <154925985+eQlenT@users.noreply.github.com>
2024-07-02 22:43:24 +0300

    refactor getTask second time

--- cmd/handlers/getTask.go
+++ cmd/handlers/getTask.go
@@ -17,58 +17,53 @@ import (
 )
 
 func GetTask(w http.ResponseWriter, r *http.Request) {
-	var errStr models.MyErr
 	tasks := make(map[string][]models.Task)
-
-	if r.Method != http.MethodGet {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		errStr.Error = "request method must be GET"
-		http.Error(w, fmt.Sprintf(`{"error": "%s"}`, errStr.Error), http.StatusBadRequest)
-		return
-	}
 
 	db, err := sql.Open("sqlite", "scheduler.db")
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		errStr.Error = fmt.Sprint(err)
-		http.Error(w, fmt.Sprintf(`{"error": "%s"}`, errStr.Error), http.StatusInternalServerError)
-		return
+		fmt.Println("open db")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	defer db.Close()
 
 	rows, err := db.Query(`SELECT id, date, title, comment, repeat FROM scheduler 
-	ORDER BY date LIMIT 50`)
+	ORDER BY date`)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		errStr.Error = fmt.Sprint(err)
-		http.Error(w, fmt.Sprintf(`{"error": "%s"}`, errStr.Error), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer rows.Close()
 
 	for rows.Next() {
 		task := models.Task{}
+
 		err := rows.Scan(&task.ID, &task.Date, &task.Title, &task.Comment, &task.Repeat)
 		if err != nil {
-			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-			errStr.Error = fmt.Sprint(err)
-			http.Error(w, fmt.Sprintf(`{"error": "%s"}`, errStr.Error), http.StatusBadRequest)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		if err := rows.Err(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 		tasks["tasks"] = append(tasks["tasks"], task)
+
 	}
+
+	// Если задач нет, возвращаем пустой json.
 	if tasks["tasks"] == nil {
 		tasks["tasks"] = []models.Task{}
 	}
 
 	response, err := json.Marshal(tasks)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		errStr.Error = fmt.Sprint(err)
-		http.Error(w, fmt.Sprintf(`{"error": "%s"}`, errStr.Error), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	w.Write(response)
 }
