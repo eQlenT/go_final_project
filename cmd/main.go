f9e66eaf2b89658998f485d0697dfddae50512f4
ibratash <154925985+eQlenT@users.noreply.github.com>
2024-07-06 20:44:27 +0300

    replace defer

--- cmd/main.go
+++ cmd/main.go
@@ -19,6 +19,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.Close()
 	DBconnection := &handlers.DBConnection{DB: db}
 	if install {
 		DBconnection.InitDB()
@@ -39,5 +40,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 }
